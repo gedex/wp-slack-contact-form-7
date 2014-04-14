c0c9ae1161bf763fd98988cbf4034732175a0226
Akeda Bagus <akeda@x-team.com>
2014-04-14 16:53:29 +0700

    Init.

---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== Slack Contact Form 7 ===
+Contributors:      akeda
+Donate link:       http://goo.gl/DELyuR
+Tags:              slack, api, chat, notification, contact, form, cf7, contact-form-7
+Requires at least: 3.6
+Tested up to:      3.8.1
+Stable tag:        trunk
+License:           GPLv2 or later
+License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+
+Send notifications to Slack channels whenever someone sent message through Contact Form 7.
+
+== Description ==
+
+This plugin is an extension to [Slack plugin](http://wordpress.org/plugins/slack) that allows you to send notifications to Slack channels whenever someone sent message through Contact Form 7.
+
+The new event will be shown on integration setting with text **When someone sent message through Contact Form 7**. If checked then notification will be delivered after Contact Form 7 sent the message through email.
+
+You can alter the message with `slack_wpcf7_submit_message` filter. The filter receives following parameters (ordered by position):
+
+* `$message` &mdash; Default message to send to Slack
+* `$form` &mdash; Form object
+* `$result` &mdash; Array of result
+
+**Development of this plugin is done on [GitHub](https://github.com/gedex/wp-slack-contact-form-7). Pull requests are always welcome**.
+
+== Installation ==
+
+1. You need to install and activate both Contact Form 7 and [Slack](http://wordpress.org/plugins/slack) plugins first.
+1. Then upload **Slack Contact Form 7** plugin to your blog's `wp-content/plugins/` directory and activate.
+1. You will see new event type with text **When someone sent message through Contact Form 7** in integration setting. If checked then notification will be delivered after Contact Form 7 sent the message through email.
+
+== Screenshots ==
+
+1. Event option in integration setting
+1. Your channel get notified when someone sent message through your Contact Form 7
+
+== Changelog ==
+
+= 0.1.0 =
+Initial release
---
+++ slack-contact-form-7.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Plugin Name: Slack Contact Form 7
+ * Plugin URI: https://github.com/gedex/wp-contact-form-7
+ * Description: This plugin allows you to send notifications to Slack channels whenever someone sent message through Contact Form 7.
+ * Version: 0.1.0
+ * Author: Akeda Bagus
+ * Author URI: http://gedex.web.id
+ * Text Domain: slack
+ * Domain Path: /languages
+ * License: GPL v2 or later
+ * Requires at least: 3.6
+ * Tested up to: 3.8
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+/**
+ * Adds new event that send notification to Slack channel
+ * when someone sent message through Contact Form 7.
+ *
+ * @param  array $events
+ * @return array
+ *
+ * @filter slack_get_events
+ */
+function wp_slack_wpcf7_submit( $events ) {
+	$events['wpcf7_submit'] = array(
+		// Action in Gravity Forms to hook in to get the message.
+		'action' => 'wpcf7_submit',
+
+		// Description appears in integration setting.
+		'description' => __( 'When someone sent message through Contact Form 7', 'slack' ),
+
+		// Message to deliver to channel. Returns false will prevent
+		// notification delivery.
+		'message' => function( $form, $result ) {
+
+			// @todo: Once attachment is supported in Slack
+			// we can send payload with nicely formatted message
+			// without relying on mail_sent result.
+			if ( $result['mail_sent'] ) {
+				return apply_filters( 'slack_wpcf7_submit_message',
+					sprintf(
+						__( 'Someone just sent a message through *%s* _Contact Form 7_. Check your email!', 'slack' ),
+						$form->title
+					),
+
+					$form,
+					$result
+				);
+			}
+
+			return false;
+		}
+	);
+
+	return $events;
+}
+add_filter( 'slack_get_events', 'wp_slack_wpcf7_submit' );
